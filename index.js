ddb735bd433376cc2e59e0791ce5c68c4d81d00b
Spark-Things <makeitspark69@gmail.com>
2021-08-24 15:34:28 +0530

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+
+var slideIndex = [1,1];
+var slideId = ["slides1","slides2"]
+showSlides(1,0);
+showSlides(1,1)
+function plusSlides(n,no) {
+  showSlides(slideIndex[no] += n,no);
+}
+
+function currentSlide(n,no) {
+  showSlides(slideIndex[no] = n,no);
+}
+
+function showSlides(n,no){
+  var i;
+  var slides = document.getElementsByClassName(slideId[no]);
+  if (n > slides.length) {
+    slideIndex[no] = 1;
+  }
+  if (n< 1) {
+    slideIndex[no] = slides.length;
+  }
+  for (let i = 0; i< slides.length; i++) {
+    slides[i].style.display = "none"
+  }
+  slides[slideIndex[no]-1].style.display = "block";
+}
